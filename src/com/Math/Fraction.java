2151398e6d5ed6d689fbbc67dad5dc33f1af4738
Dominic Z <dominiczimmer@online.de>
2015-03-04 14:47:25 +0100

    Added doc to add()

--- src/com/Math/Fraction.java
+++ src/com/Math/Fraction.java
@@ -5,6 +5,7 @@ public class Fraction {
 	private int num;
 	private int den;
 
+	
 	Fraction(int arg0, int arg1) {
 		this.num = arg0;
 		this.den = arg1;
@@ -15,9 +16,14 @@ public class Fraction {
 		this.den = 1;
 	}
 
-	public Fraction add(Fraction f) {
-		int n = this.num * f.den + this.den * f.num;
-		int d = this.den * f.den;
+	/**
+	 * Addition of two Fractions
+	 * @param addend Addend of the addition
+	 * @return Sum of fractions
+	 * */
+	public Fraction add(Fraction addend) {
+		int n = this.num * addend.den + this.den * addend.num;
+		int d = this.den * addend.den;
 		this.num = n;
 		this.den = d;
 		this.cancel();
