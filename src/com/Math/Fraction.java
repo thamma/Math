a4fcad4b1f65be5f25b49eee027a810c9cd151c0
Gregory Stock <cubegalaxy@gmx.de>
2015-03-04 14:32:47 +0100

    Added Comments

--- src/com/Math/Fraction.java
+++ src/com/Math/Fraction.java
@@ -5,16 +5,19 @@ public class Fraction {
 	private int num;
 	private int den;
 
+	// Konstruktor Bruch
 	Fraction(int arg0, int arg1) {
 		this.num = arg0;
 		this.den = arg1;
 	}
 
+	// Konstruktor ganze Zahl
 	Fraction(int arg0) {
 		this.num = arg0;
 		this.den = 1;
 	}
 
+	// Addition
 	public Fraction add(Fraction f) {
 		int n = this.num * f.den + this.den * f.num;
 		int d = this.den * f.den;
@@ -24,6 +27,7 @@ public class Fraction {
 		return this;
 	}
 
+	// Subtraktion
 	public Fraction sub(Fraction f) {
 		int n = this.num * f.den - this.den * f.num;
 		int d = this.den * f.den;
@@ -32,18 +36,21 @@ public class Fraction {
 		this.cancel();
 		return this;
 	}
-	
+
+	// Multiplikation
 	public Fraction mul(Fraction f) {
 		this.num = this.num * f.num;
 		this.den = this.den * f.den;
 		this.cancel();
 		return this;
 	}
-	
+
+	// Division
 	public Fraction div(Fraction f) {
 		return this.mul(f.inverse());
 	}
-	
+
+	// Kehrbruch
 	public Fraction inverse() {
 		int x = this.num;
 		this.num = this.den;
@@ -51,22 +58,25 @@ public class Fraction {
 		return this;
 	}
 
+	// Konvertierung zu String
 	public String toString() {
 		return this.num + "/" + this.den;
 	}
 
+	// Bruch kürzen
 	private Fraction cancel() {
 		int ggt = gcd(this.num, this.den);
-		this.num = this.num/ggt;
-		this.den = this.den/ggt;
+		this.num = this.num / ggt;
+		this.den = this.den / ggt;
 		return this;
 	}
 
+	// Größter gemeinsamer Teiler
 	private static int gcd(int a, int b) {
-		if(a == b){
+		if (a == b) {
 			return a;
-		}else{
-			return gcd(b, b-(a%b));
+		} else {
+			return gcd(b, b - (a % b));
 		}
 	}
 }
